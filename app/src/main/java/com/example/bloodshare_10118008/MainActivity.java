771c6effb276e47221b05d1c617bd28ac061e78b
Nagaku <mimelnagaku1@gmail.com>
2021-04-29 23:09:31 +0700

    Final commmit

--- app/src/main/java/com/example/bloodshare_10118008/MainActivity.java
+++ app/src/main/java/com/example/bloodshare_10118008/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.bloodshare_10118008;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String nama = "";
+    public String tipe = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pertama(View view) {
+        setContentView(R.layout.activity_form);
+    }
+
+    public void kedua(View view) {
+        setContentView(R.layout.activity_almost_there);
+    }
+
+    public void ketiga(View view) {
+        setContentView(R.layout.activity_verify_account);
+    }
+
+    public void keempat(View view) {
+        setContentView(R.layout.activity_profile);
     }
 }
